f3709c2445fe2dc76f8cae5e4f7fd05b6f0f01c8
jplendor <jplendor@gmail.com>
2022-05-19 15:55:36 +0900

    remove unnecessary files & add router logic

---
+++ src/pages/Diary.js
@@ -0,0 +1,15 @@
+import { useParams } from "react-router-dom";
+
+const Diary = () => {
+    const { id } = useParams();
+
+    return (
+        <div>
+            <h1>Diary</h1>
+            <p>이곳은 Diary 입니다.</p>
+            <p>{id}</p>
+        </div>
+    );
+};
+
+export default Diary;
