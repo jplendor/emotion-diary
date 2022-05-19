1f163f26bd3abe87eaf46fcfc2f45e58523b3211
jplendor <jplendor@gmail.com>
2022-05-19 19:48:14 +0900

    add Home Header

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,39 @@
+import { useState } from "react";
+
+import MyHeader from "../components/MyHeader";
+import MyButton from "../components/MyButton";
+
 const Home = () => {
+    const [curDate, setCurDate] = useState(new Date());
+    const headerText = `${curDate.getFullYear()}년 ${curDate.getMonth() + 1}월`;
+
+    const decreaseMonth = () => {
+        setCurDate(
+            new Date(
+                curDate.getFullYear(),
+                curDate.getMonth() - 1,
+                curDate.getDate()
+            )
+        );
+    };
+
+    const increaseMonth = () => {
+        setCurDate(
+            new Date(
+                curDate.getFullYear(),
+                curDate.getMonth() + 1,
+                curDate.getDate()
+            )
+        );
+    };
+
     return (
         <div>
-            <h1>Home</h1>
-            <p>이곳은 Home 입니다.</p>
+            <MyHeader
+                headerText={headerText}
+                leftChild={<MyButton text={"<"} onClick={decreaseMonth} />}
+                rightChild={<MyButton text={">"} onClick={increaseMonth} />}
+            />
         </div>
     );
 };
